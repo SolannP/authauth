7d5b979cb3ec6768533343d5b8aad6fbd08c6ee7
Solann PUYGRENIER <spuygrenier@cfai-formation.com>
2022-11-07 20:39:06 +0100

    add api to catalogue

--- AuthAuthMicroService/Controllers/UserManagementController.cs
+++ AuthAuthMicroService/Controllers/UserManagementController.cs
@@ -18,8 +18,8 @@ public class UserManagementController : ControllerBase
         BuilderApplicationService builder = new BuilderApplicationService();
 
         builder.BuildRedisDataBaseConnection(
-            redisData.GetSection("User").Value, 
-            redisData.GetSection("Password").Value, 
+            redisData.GetSection("User").Value,
+            redisData.GetSection("Password").Value,
             redisData.GetSection("Endpoint").Value
         );
         accountManager = builder.Make();
@@ -36,33 +36,93 @@ public class UserManagementController : ControllerBase
     /// <response code="500">Something went wrong on the server side</response>
     /// <remarks>Use redis database for performance, please do not create to much user</remarks>
     /// <exception cref="ArgumentOutOfRangeException">Rare case when result is in unknow state, i.e. not among Created|AlreadyExisting|Error </exception>
-    [HttpPost("create")]
-    public IActionResult Create(string? contact,[Required] string login , [Required] string password)
+    [HttpPost("user")]
+    public IActionResult Create(string? contact, [Required] string login, [Required] string password)
     {
         AccountCreationStatus status;
         status = contact is null ? accountManager.Create(login, password) : accountManager.Create(contact, login, password);
 
         switch (status)
         {
-            case AccountCreationStatus.Created: return Created("account","SUCCES");
-            case AccountCreationStatus.AlreadyExisting: return Conflict("Already existing user");
-            case AccountCreationStatus.Error: return StatusCode(500);
-            default: throw new ArgumentOutOfRangeException("AccountCreationStatus not in possible value");
+            case AccountCreationStatus.Created:
+                return Created("account", "SUCCES");
+            case AccountCreationStatus.AlreadyExisting:
+                return Conflict("Already existing user");
+            case AccountCreationStatus.Error:
+                return StatusCode(500);
+            default:
+                throw new ArgumentOutOfRangeException("AccountCreationStatus not in possible value");
         }
     }
 
-    [HttpGet("correct")]
-    public IActionResult Correct(string? contact, [Required] string login, [Required] string password)
+    /// <summary>
+    /// Api dediacted to the credential verification
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="password"></param>
+    /// <param name="contact"></param>
+    /// <response code="200">Correct credential creation</response>
+    /// <response code="403">Wrong credential</response>
+    /// <response code="500">Something went wrong on the server side</response>
+    /// <remarks>⚠If user have been created with contact data, then contact data become mandatory for correct validation</remarks>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    [HttpGet("user")]
+    public IActionResult Correct([Required] string login, [Required] string password, string? contact)
     {
         AccountAccesStatus status;
         status = contact is null ? accountManager.IsCorrectPassword(login, password) : accountManager.IsCorrectPassword(contact, login, password);
-        
+
         switch (status)
         {
-            case AccountAccesStatus.CorrectCredential:return Ok("SUCCES");
-            case AccountAccesStatus.IncorectCredential: return BadRequest();
-            case AccountAccesStatus.Error: return StatusCode(500);
-            default:throw new ArgumentOutOfRangeException("AccountAccesStatus not in possible value");
+            case AccountAccesStatus.CorrectCredential:
+                return Ok("SUCCES");
+            case AccountAccesStatus.IncorectCredential:
+                return Forbid("Wrong credential provided");
+            case AccountAccesStatus.Error:
+                return StatusCode(500);
+            default:
+                throw new ArgumentOutOfRangeException("AccountAccesStatus not in possible value");
         }
     }
+
+    /// <summary>
+    ///  Delete : In progress
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="password"></param>
+    /// <param name="contact"></param>
+    /// <returns></returns>
+    [HttpDelete("user")]
+    public IActionResult Delete([Required] string login, [Required] string password, string? contact)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    ///  GetAllUser : In progress
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="password"></param>
+    /// <param name="contact"></param>
+    /// <returns></returns>
+    [HttpGet("user/all")]
+    public IActionResult GetAllUser([Required] string login, [Required] string password, string? contact)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    ///  AdminDelete : In progress
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="password"></param>
+    /// <param name="contact"></param>
+    /// <returns></returns>
+    [HttpDelete("user")]
+    public IActionResult AdminDelete([Required] string login, [Required] string password, string? contact)
+    {
+        return Ok();
+    }
+
+
 }
